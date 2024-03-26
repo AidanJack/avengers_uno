a72ceacc5d66d7daf26c236df677ee5d340260b7
Sandy Lao <sandy.lao@mail.mcgill.ca>
2024-03-26 15:25:28 -0400

    Add home page with Play button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,18 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <h1 className="text-center font-cinzel text-white text-6xl h-50 p-20">Avengers Uno</h1>
-      <button className="btn-gray">PLAY</button>
+      <h1 className="text-center font-cinzel text-white bg-black text-5xl h-30 p-4">
+        Avengers Uno
+      </h1>
+
+    <div className="box flex flex-col items-center h-screen justify-center">
+        <h1 className="text-center font-cinzel text-white text-7xl h-50">Avengers Uno</h1>
+        <button className="text-white bg-black hover:bg-gray-900 font-medium font-cinzel py-2 px-20 mt-20 rounded text-center">
+          PLAY
+        </button> 
+      </div>
     </div>
   );
 }
